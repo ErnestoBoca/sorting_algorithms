f778ad563a389d2db208152d7cf4643da929df2f
Ernesto Boca <boca.ernesto13@gmail.com>
2024-01-21 11:41:33 -0800

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, swapped;
+	int aux;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+
+		if (!swapped)
+			break;
+	}
+}
