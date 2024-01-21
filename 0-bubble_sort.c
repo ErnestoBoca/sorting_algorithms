93c3376f909a8b1d197d759ca09248d070a2623d
Ernesto Boca <boca.ernesto13@gmail.com>
2024-01-21 12:49:03 -0800

    task 0 efficiency fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, swapped;
 	int aux;
 
-	if (array == NULL || size <= 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
